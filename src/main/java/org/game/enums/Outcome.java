203de803e092cb156747d62f6f1b2004dbb563dc
rwilliams <richyrichw93@gmail.com>
2024-07-04 10:24:26 +0200

    moved messages to message constants

--- src/main/java/org/game/enums/Outcome.java
+++ src/main/java/org/game/enums/Outcome.java
@@ -1,15 +1,14 @@
 package org.game.enums;
 
-import org.game.constants.Constants;
 import org.game.constants.MessageConstants;
 
 /**
  * Enum representing the possible outcomes of a game round.
  */
 public enum Outcome {
-    USER_WIN(Constants.USER_WIN_MSG),
-    COMPUTER_WIN(Constants.COMPUTER_WINS_MSG),
-    DRAW(Constants.DRAW_MSG);
+    USER_WIN(MessageConstants.USER_WIN_MSG),
+    COMPUTER_WIN(MessageConstants.COMPUTER_WINS_MSG),
+    DRAW(MessageConstants.DRAW_MSG);
     
     private final String message;
     
