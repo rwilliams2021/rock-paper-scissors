43bae464560bcf7aec4550ee50951704a5db6b40
rwilliams <richyrichw93@gmail.com>
2024-07-04 10:24:13 +0200

    moved messages to message constants

--- src/main/java/org/game/constants/Constants.java
+++ src/main/java/org/game/constants/Constants.java
@@ -3,7 +3,4 @@ package org.game.constants;
 public class Constants {
     public static final String COMPUTER = "computer";
     public static final String USER = "user";
-    public static final String USER_WIN_MSG = "You win!";
-    public static final String COMPUTER_WINS_MSG = "Computer wins!";
-    public static final String DRAW_MSG = "It's a draw!";
 }
